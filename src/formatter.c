381d6ecf148e6a3a81523809bb4e8627c853cc16
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2023-02-19 18:18:23 +0100

    Type byte is changed to c type char

--- src/formatter.c
+++ src/formatter.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "formatter.h"
 
+struct TypeMapEntry {
+    const char *inName;
+    const char *outName;
+    struct TypeMapEntry *next;
+};
+
+static const char *find(struct TypeMapEntry *map, const char *inName) {
+    while (map != NULL) {
+        if (strcmp(inName, map->inName) == 0) {
+            return map->outName;
+        }
+
+        map = map->next;
+    }
+
+    return NULL;
+}
+
 int format(const struct StructEntry *entry) {
+    struct TypeMapEntry *typeMap = malloc(sizeof(struct TypeMapEntry));
+    typeMap->inName = "byte";
+    typeMap->outName = "char";
+    typeMap->next = NULL;
+
     if (entry->name != NULL) {
+        const char * const outType = find(typeMap, entry->type);
+        if (outType == NULL) {
+            fprintf(stderr, "Undefined type %s", entry->type);
+            free(typeMap);
+            return 1;
+        }
+
         if (entry->count >= 2) {
-            printf("%s[%u] %s\n", entry->type, entry->count, entry->name);
+            printf("%s[%u] %s\n", outType, entry->count, entry->name);
         }
         else {
-            printf("%s %s\n", entry->type, entry->name);
+            printf("%s %s\n", outType, entry->name);
         }
     }
 
+    free(typeMap);
     return 0;
 }
